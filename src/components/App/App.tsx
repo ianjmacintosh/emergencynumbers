cb4daff873c6a8995b6d0e30aebbbef471e1eb68
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-06 23:25:52 +0000

    Add a blurb explaining the page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,6 +9,15 @@ function App() {
   return (
     <div>
       <h1>Emergency Numbers</h1>
+      <p>
+        Here are the official published emergency phone numbers you can call if
+        you are in a foreign country and need help.
+      </p>
+      <p>
+        Calling a local emergency service is usually best, but calling anyone is
+        better than calling no one.
+      </p>
+      <p>You will look back on what you did. Don't panic, be a hero.</p>
 
       {SUPPORTED_COUNTRIES.map(({ id, name }) => {
         const services = SERVICES[id];
