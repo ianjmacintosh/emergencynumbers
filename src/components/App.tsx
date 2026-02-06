17d2704d01cba2e7cc53cc11fbe661e3415fdb16
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-06 18:51:37 +0000

    Update homepage to list phone numbers
    This change also removes 2 unused images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,77 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+type CountryInfo = { name: string; services: Service[] };
+type Service = {
+  type: string;
+  name: string;
+  description?: string;
+  phoneNumber: string;
+};
 
+const COUNTRIES: CountryInfo[] = [
+  {
+    name: "Brazil",
+    services: [
+      {
+        type: "Ambulance",
+        name: "Emergency services",
+        description: "SAMU ambulance service",
+        phoneNumber: "192",
+      },
+      /*
+      TODO: Add these services:
+        190 - Policia (Police)
+        191 - Policia Rodoviaria (Police on interstate roads)
+        192 - Ambulancia (Ambulance)
+        193 - Bombeiros (Fire Department)
+      */
+    ],
+  },
+  {
+    name: "United States",
+    services: [
+      {
+        type: "Switchboard",
+        name: "Emergency services",
+        description: "Switchboard for various services",
+        phoneNumber: "911",
+      },
+    ],
+  },
+];
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Emergency Numbers</h1>
+
+      {COUNTRIES.map((country) => (
+        <CountryCard {...country} />
+      ))}
+    </div>
+  );
+}
+
+function CountryCard({ name, services }: CountryInfo) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      {services.map((service: Service) => {
+        const { name, phoneNumber } = service;
+
+        return (
+          <dl>
+            <dt>{name}</dt>
+            <dd>
+              <a href={`tel:${phoneNumber}`}>{phoneNumber}</a> (
+              <a href={`tel:${phoneNumber}`}>Call</a>)
+            </dd>
+          </dl>
+        );
+      })}
+    </div>
   );
 }
 
