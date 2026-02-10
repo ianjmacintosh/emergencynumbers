0e3be563e5d596949d48e89acb153d2300d73f16
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-10 19:44:26 +0000

    Sort services by dispatch first, then phone number

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -14,9 +14,18 @@ function CountryCard({
     <div className={styles.countryCard}>
       <h2>{name}</h2>
       <ul className={styles.services}>
-        {services.map((service: Service) => (
-          <ServiceCard service={service} key={service.phoneNumber} />
-        ))}
+        {services
+          .sort((service1, service2) => {
+            if (service1.type === "Dispatch" || service2.type === "Dispatch") {
+              return -1;
+            }
+            return (
+              parseInt(service1.phoneNumber) - parseInt(service2.phoneNumber)
+            );
+          })
+          .map((service: Service) => (
+            <ServiceCard service={service} key={service.phoneNumber} />
+          ))}
       </ul>
     </div>
   );
