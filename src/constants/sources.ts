b025b3b175115006a61a42317b21b010553cd786
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-13 22:52:48 +0000

    Update data and update consumers

---
+++ src/constants/sources.ts
@@ -0,0 +1,14 @@
+export type Source = {
+  name: string;
+  articleName: string;
+  url: string;
+  lastUpdatedAt: string;
+};
+
+export const sources = {
+  "d9d95748-7633-4611-b710-b0612620a042": {
+    name: "ITU-T",
+    articleName: "E.129 Important numbers",
+    url: "https://www.itu.int/net/itu-t/inrdb/e129_important_numbers.aspx",
+  },
+};
