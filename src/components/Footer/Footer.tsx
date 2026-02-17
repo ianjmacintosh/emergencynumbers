4f5914480da156f6eaa0dee7709e1822904e028f
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-17 18:59:29 +0000

    Update footer copyright notice

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,14 +3,16 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <div className={styles.footer}>
+      <p>&copy; 2026 Ian J. MacIntosh</p>
       <p>
-        Phone numbers provided from multiple sources, including{" "}
+        Phone number content from{" "}
         <a
           href="https://www.itu.int/en/about/Pages/terms-of-use.aspx"
           target="_blank"
         >
           ITU-T E.129
-        </a>
+        </a>{" "}
+        used with permission for non-commercial purposes
       </p>
     </div>
   );
