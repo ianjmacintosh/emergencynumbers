2f3e668462bf54eb49b1853d30b459fd0196415a
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-19 01:17:47 +0000

    Sort numbers

--- src/components/CountryCard/helpers.ts
+++ src/components/CountryCard/helpers.ts
@@ -12,26 +12,53 @@ Card:
 
 export function getServiceCardData(services: Service[]) {
   const cards: Card[] = [];
-  // This function should return an array of card data
-  // Each card must have exactly one phone number
-  // Each card must have exactly one title
-  // Each card must have exactly one type
-  // Each card must include at least one service
-  // Each card may have an extended description
 
   // First we build all the cards using service data
   for (const service of services) {
     const { type, phoneNumber, name, description } = service;
+
     // Find the card
-    cards.push({
-      type,
-      phoneNumber,
-      name,
-      description,
-    });
+    const currentCard = cards.find(
+      ({ phoneNumber: currentPhoneNumber }) =>
+        phoneNumber === currentPhoneNumber,
+    );
+    if (currentCard) {
+      currentCard.type = "General";
+      currentCard.services.push(service);
+    } else {
+      cards.push({
+        type,
+        phoneNumber,
+        name,
+        description,
+        services: [service],
+      });
+    }
   }
 
   // Then we sort all the cards
+  cards.sort((a, b) => {
+    const tier1 = ["General", "Dispatch"];
+    const tier2 = ["Fire Department", "Ambulance", "Police"];
+
+    // A comes first if it's in a higher tier than B
+    if (
+      (tier1.includes(a.type) && !tier1.includes(b.type)) ||
+      (tier2.includes(a.type) && !tier2.includes(b.type))
+    ) {
+      return -1;
+    }
+    // B comes first if it's in a higher tier than A
+    if (
+      (tier1.includes(b.type) && !tier1.includes(a.type)) ||
+      (tier2.includes(b.type) && !tier2.includes(a.type))
+    ) {
+      return 1;
+    }
+
+    // Same tier: sort by phone number ascending
+    return Number(a.phoneNumber) - Number(b.phoneNumber);
+  });
 
   // Then we return the array of sorted cards
   return cards;
--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -11,21 +11,25 @@ import {
   CopyIcon,
 } from "@phosphor-icons/react";
 
-import type { ServiceType } from "../../constants/emergency-services";
+import type { Service, ServiceType } from "../../constants/emergency-services";
+
+export type CardType = "General" | ServiceType;
 
 export type Card = {
   phoneNumber: string;
   name: string;
-  type: ServiceType;
+  type: CardType;
   description?: string;
+  services: Service[];
 };
 
 import styles from "./ServiceCard.module.css";
 import VisuallyHidden from "../VisuallyHidden";
 
 function ServiceCard({ service }: { service: Card }) {
-  const { phoneNumber, type, description } = service;
+  const { phoneNumber, type, description, services } = service;
   const Icon = {
+    General: HeadsetIcon,
     Dispatch: HeadsetIcon,
     Ambulance: AmbulanceIcon,
     "Fire Department": FireTruckIcon,
@@ -43,31 +47,66 @@ function ServiceCard({ service }: { service: Card }) {
           <div className={styles.iconWrapper}>
             <Icon size={32} weight="fill" className={styles.icon} />
           </div>
-          <span className={styles.type}>{type}</span>
-        </div>
+          {type === "General" ? (
+            <>
+              <span className={styles.type}>{phoneNumber}</span>
 
+              <button
+                className={styles.copyButton}
+                onClick={() => {
+                  copyToClipboard(phoneNumber);
+                }}
+              >
+                <CopyIcon
+                  size={24}
+                  weight="regular"
+                  className={`${styles.icon} ${styles.secondaryIcon}`}
+                >
+                  <VisuallyHidden>
+                    Copy {phoneNumber} to Clipboard
+                  </VisuallyHidden>
+                </CopyIcon>
+              </button>
+            </>
+          ) : (
+            <span className={styles.type}>{type}</span>
+          )}
+        </div>
         {description && (
           <p className={styles.descriptionWrapper}>{description}</p>
         )}
-        <div className={styles.phoneNumberWrapper}>
-          <span>{phoneNumber}</span>
+        {services.length > 1 && (
+          <>
+            Services offered:
+            <ul>
+              {services.map((currentService) => (
+                <li>{currentService.name}</li>
+              ))}
+            </ul>
+          </>
+        )}
+        {type === "General" ? undefined : (
+          <div className={styles.phoneNumberWrapper}>
+            <span>{phoneNumber}</span>
 
-          <button
-            className={styles.copyButton}
-            onClick={() => {
-              copyToClipboard(phoneNumber);
-            }}
-          >
-            <CopyIcon
-              size={24}
-              weight="regular"
-              className={`${styles.icon} ${styles.secondaryIcon}`}
+            <button
+              className={styles.copyButton}
+              onClick={() => {
+                copyToClipboard(phoneNumber);
+              }}
             >
-              <VisuallyHidden>Copy {phoneNumber} to Clipboard</VisuallyHidden>
-            </CopyIcon>
-          </button>
-        </div>
+              <CopyIcon
+                size={24}
+                weight="regular"
+                className={`${styles.icon} ${styles.secondaryIcon}`}
+              >
+                <VisuallyHidden>Copy {phoneNumber} to Clipboard</VisuallyHidden>
+              </CopyIcon>
+            </button>
+          </div>
+        )}
       </div>
+
       <a
         className={`${styles.callButton} ${styles.iconWrapper}`}
         href={`tel:${phoneNumber}`}
