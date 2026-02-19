9d8af7f2c0bb3ed6c1e6f8a7e189c6e1496e8f80
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-19 23:15:38 +0000

    Do some word smithing

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -9,31 +9,28 @@ function AboutPage() {
         <h1>About</h1>
         <p>
           At about 11:30pm on a random Sunday night, my wife got an emergency
-          call from a family member in distress; they were far away, immobile,
-          and needed urgent medical help. Someone from the US might leap to call
-          911, but some countries use other phone numbers.
+          call from a family member in distress outside the US; they were
+          immobile and needed urgent medical help. 911 isn't universal, so we
+          didn't know what phone number to call.
         </p>
         <h3>
-          This web site is to help people get in touch with emergency services,
-          anywhere in the world.
+          I built this web site to help people get in touch with emergency
+          services, anywhere in the world.
         </h3>
         <p>
-          It's awful not knowing how to call an ambulance when you really need
-          one, so I made this tool to help anyone in a similar situation. In a
-          moment of crisis, the last thing I want to be doing is messing around
-          on Google trying to find an up-to-date emergency services phone
-          number.
-        </p>
-        <p>
-          We didn't know the phone number for emergency assistance, so my wife
-          called a friend in that area who works as both an emergency room
-          doctor as well as an EMT. She arrived at our relative's home in 15
-          minutes and provided life-saving treatment for what turned out to be
-          sepsis -- typically fatal if left to run its course. Calling for help
-          saved a life.
+          It's awful not knowing how to call an ambulance when you{" "}
+          <em>really</em> need one, so I made this tool to help anyone in a
+          similar situation. In a moment of crisis, the last thing I want to be
+          doing is messing around on Google trying to find an up-to-date
+          emergency services phone number. Bookmark this site in case you need
+          it some day.
         </p>
         <h2>How many countries are supported?</h2>
-        <p>{Object.keys(SERVICES).length}</p>
+        <p>
+          {Object.keys(SERVICES).length}. This site provides emergency service
+          phone numbers for {Object.keys(SERVICES).length} different countries
+          and territories.
+        </p>
         <h2>Is this data trustworthy?</h2>
         <p>
           Yes. I pulled these numbers from the{" "}
@@ -43,15 +40,14 @@ function AboutPage() {
           >
             ITU-T E.129 Important numbers
           </a>{" "}
-          list, which is regularly updated and comes from a specialized
-          telecommunications standards agency of the United Nations.
+          list, which is regularly updated by the specialized telecommunications
+          standards agency of the United Nations.
         </p>
         <p>
           Although these numbers are accurate, there may be even better options
-          for help that aren't listed here. Calling for <em>local</em> help is
-          almost always better than going through a national call center, but
-          calling anyone is better than calling no one, especially when minutes
-          count.
+          for help that aren't listed here. Calling for local help is almost
+          always better than going through a national call center, but calling
+          anyone is better than calling no one, especially when minutes count.
         </p>
         <h2>Can I use or modify this tool's source code?</h2>
         <p>
@@ -73,6 +69,14 @@ function AboutPage() {
           grant permission to download, copy, and use content for personal,
           educational, and non-commercial use so long as you acknowledge the
           source and maintain all copyright and other proprietary notices.
+        </p>
+        <h2>So what happened with your relative?</h2>
+        <p>
+          My wife called a friend in that area who works as an emergency room
+          doctor and volunteers for EMT shifts monthly. She arrived at our
+          relative's home in 15 minutes and provided life-saving treatment for
+          what turned out to be sepsis -- typically fatal if left to run its
+          course. Calling for help saved a life.
         </p>
       </div>
       <Footer />
