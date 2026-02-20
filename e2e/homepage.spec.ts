4610d70f45a8c43fee3b0658ac30a6c2fa5f1e99
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-20 16:29:29 -0300

    23 add about page (#24)
    * Change page title
    
    * Add an about page
    
    * Update footer to include About page
    
    * Fix weirdness with light mode vs dark mode
    
    * Do some word smithing
    
    * Clean up copy
    
    * Separate question/answers
    
    * Do more wordsmithing

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("/");
 
-  await expect(page).toHaveTitle(/emergencynumbers/);
+  await expect(page).toHaveTitle("Emergency Service Phone Numbers");
 });
 
 test("has 'Call' text at least once", async ({ page }) => {
