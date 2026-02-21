5288c32143bc50a62c6ce2a836fe325e74cb0281
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-21 01:25:35 +0000

    Return user IP in geolookup API

--- worker/index.ts
+++ worker/index.ts
@@ -3,13 +3,18 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname === "/api/geo") {
-      const country = request.cf?.country ?? null;
-      return new Response(JSON.stringify({ country }), {
-        headers: {
-          "Content-Type": "application/json",
-          "Cache-Control": "no-store",
+      return new Response(
+        JSON.stringify({
+          ip: request.headers.get("CF-Connecting-IP"),
+          country: request.cf?.country ?? null,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-store",
+          },
         },
-      });
+      );
     }
 
     return new Response("Not Found", { status: 404 });
