f59cf71dda98c3aa516b4df17d4d3324454cbaac
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-26 19:05:55 +0000

    Redesign service card

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -9,6 +9,7 @@ import {
   InfoIcon,
   PhoneOutgoingIcon,
   CopyIcon,
+  CheckIcon,
 } from "@phosphor-icons/react";
 
 import type { IconProps } from "@phosphor-icons/react";
@@ -21,67 +22,72 @@ import React from "react";
 
 function ServiceCard({ service }: { service: ServiceData }) {
   const { phoneNumber, type, description } = service;
-
-  return (
-    <li className={styles.service} aria-label="Emergency Service">
-      <div className={styles.serviceInfoWrapper}>
-        <div className={styles.titleWrapper}>
-          <div className={styles.iconWrapper}>
-            <Icon type={type} size={32} weight="fill" className={styles.icon} />
-          </div>
-          <span className={styles.type}>{type}</span>
-        </div>
+  const newMarkup = (
+    <article className={styles.service} aria-label="Emergency Service">
+      <h2>
+        <Icon type={type} size={32} weight="fill" className={styles.icon} />
+        {type}
+      </h2>
+      <dl>
+        <dt>Phone Number</dt>
+        <dd>{phoneNumber}</dd>
         {description && (
-          <p className={styles.descriptionWrapper}>{description}</p>
+          <>
+            <dt>Additional Information</dt>
+            <dd>{description}</dd>
+          </>
         )}
-        <div className={styles.phoneNumberWrapper}>
-          <span>Call {phoneNumber}</span>
-
-          <CopyButton content={phoneNumber}></CopyButton>
-        </div>
-      </div>
-
-      <a
-        className={`${styles.callButton} ${styles.iconWrapper}`}
-        href={`tel:${phoneNumber}`}
-      >
-        <PhoneOutgoingIcon size={32} weight="fill" />
-        <VisuallyHidden>Call {phoneNumber}</VisuallyHidden>
-      </a>
-    </li>
+      </dl>
+      <ul className={styles.actions}>
+        <li>
+          <button>
+            <PhoneOutgoingIcon size={24} weight="fill" /> Call {phoneNumber}
+          </button>
+        </li>
+        <li>
+          <CopyButton content={phoneNumber}>
+            Copy {phoneNumber} to clipboard
+          </CopyButton>
+        </li>
+      </ul>
+    </article>
   );
+
+  return newMarkup;
 }
 
-function CopyButton({ content }: { content: string }) {
+function CopyButton({
+  content,
+  children,
+}: {
+  content: string;
+  children: React.ReactElement;
+}) {
   const [showNotification, setShowNotification] = React.useState(false);
   return (
-    <div className={styles.copyButtonWrapper}>
-      <button
-        className={styles.copyButton}
-        onClick={() => {
-          setShowNotification(true);
+    <button
+      className={styles.copyButton}
+      onClick={() => {
+        setShowNotification(true);
 
-          setTimeout(() => {
-            setShowNotification(false);
-          }, 3000);
-          copyToClipboard(content);
-        }}
-      >
-        <CopyIcon
-          weight="regular"
-          className={styles.icon}
-          height={24}
-        ></CopyIcon>
-        <VisuallyHidden>Copy {content} to Clipboard</VisuallyHidden>
-      </button>
-      <span
-        role="status"
-        aria-hidden={!showNotification}
-        className={`${styles.notification} ${showNotification ? styles.visible : undefined}`.trim()}
-      >
-        Copied
-      </span>
-    </div>
+        setTimeout(() => {
+          setShowNotification(false);
+        }, 3000);
+        copyToClipboard(content);
+      }}
+    >
+      {showNotification ? (
+        <>
+          <CheckIcon size={24} />
+          Copied "{content}"
+        </>
+      ) : (
+        <>
+          <CopyIcon size={24} weight="fill" />
+          {children}
+        </>
+      )}
+    </button>
   );
 }
 
