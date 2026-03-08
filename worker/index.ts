2499193972e546b16ce4e54063e9316627578f2d
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-03-08 17:44:54 +0000

    Route requests for root to locale

--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,15 @@
+import { COUNTRY_NAMES } from "../src/constants";
+
 export default {
   async fetch(request: Request): Promise<Response> {
     const url = new URL(request.url);
+    const userCountry = request.cf?.country ?? null;
 
     if (url.pathname === "/api/geo") {
       return new Response(
         JSON.stringify({
           ip: request.headers.get("CF-Connecting-IP"),
-          country: request.cf?.country ?? null,
+          country: userCountry,
         }),
         {
           headers: {
@@ -15,6 +18,20 @@ export default {
           },
         },
       );
+    }
+
+    if (url.pathname === "/") {
+      return new Response(null, {
+        status: 302,
+        headers: {
+          Location:
+            typeof userCountry === "string" && userCountry in COUNTRY_NAMES
+              ? `/${userCountry.toLowerCase()}/`
+              : `/us/`,
+
+          "Cache-Control": "no-cache, no-store",
+        },
+      });
     }
 
     return new Response("Not Found", { status: 404 });
