9551ea2c57c9ea840d47645124641bb2e3469639
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-04-03 16:22:53 +0000

    Check for agreement values more safely

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -85,6 +85,7 @@ function App({ initialCountry }: { initialCountry?: string }) {
           userLocation in SERVICES &&
           userLocation !== currentCountryId &&
           suppressBanner === false &&
+          agreedToTerms &&
           agreedToTerms !== "never" && (
             <Banner>
               <h2>Need info for {COUNTRY_NAMES[userLocation]}?</h2>
@@ -127,8 +128,7 @@ function App({ initialCountry }: { initialCountry?: string }) {
           )}
         <div className="content-wrapper">
           <h1>Emergency Service Phone Numbers</h1>
-
-          {agreedToTerms !== "never" ? (
+          {agreedToTerms && agreedToTerms !== "never" && (
             <CountrySelect
               value={currentCountryId}
               onChange={(value) => {
@@ -136,7 +136,9 @@ function App({ initialCountry }: { initialCountry?: string }) {
                 setCurrentCountryId(value as keyof typeof SERVICES);
               }}
             />
-          ) : (
+          )}
+
+          {agreedToTerms && agreedToTerms === "never" && (
             <Disclaimer
               agree={() => {
                 setAgreedToTerms(Date.now().toString());
@@ -146,7 +148,7 @@ function App({ initialCountry }: { initialCountry?: string }) {
           )}
         </div>
       </header>
-      {agreedToTerms !== "never" && (
+      {agreedToTerms && agreedToTerms !== "never" && (
         <main className="content-wrapper">
           <CountryCard id={currentCountryId} />
         </main>
